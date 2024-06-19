06ebb0aca216cf22011df6702ce5724e37133908
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-06-19 14:10:48 +0900

    editのメソッドを外部関数化

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f76276715926a1a7040c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fa4a6e3664fe7622e9f3');
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { registerBlockType } from "@wordpress/blocks";
-import { useBlockProps, InspectorControls } from "@wordpress/block-editor";
-import { useSelect } from "@wordpress/data";
-import { PanelBody, RangeControl, ToggleControl } from "@wordpress/components";
 import { __ } from "@wordpress/i18n";
+import MyCustomBlockEdit from "./edit";
 import "./editor.css";
 import "./style.css";
 
@@ -31,48 +29,7 @@ registerBlockType("my-custom/block", {
             default: true,
         },
     },
-    edit({ attributes, setAttributes, clientId }) {
-        const blockProps = useBlockProps({ style: { fontSize: attributes.fontSize } });
-
-        const recentPosts = useSelect((select) => {
-            const posts = select("core").getEntityRecords("postType", "post", {
-                per_page: 5,
-                _embed: true,
-            });
-
-            if (posts) {
-                return posts.map((post) => {
-                    const categories = post._embedded?.["wp:term"]?.[0];
-                    return {
-                        ...post,
-                        categories: categories ? categories.map((category) => category.name).join(", ") : "",
-                    };
-                });
-            }
-            return [];
-        }, []);
-
-        return (
-            <div {...blockProps}>
-                <InspectorControls>
-                    <PanelBody title={__("Font Size")}>
-                        <RangeControl label={__("Font Size")} value={attributes.fontSize} onChange={(fontSize) => setAttributes({ fontSize })} min={12} max={36} />
-                        <ToggleControl label={__("Display Date")} checked={attributes.date} onChange={(date) => setAttributes({ date })} />
-                    </PanelBody>
-                </InspectorControls>
-                <ul>
-                    {recentPosts &&
-                        recentPosts.map((post) => (
-                            <li key={post.id}>
-                                {attributes.date && <span className="my-custom-block-post-date">{new Date(post.date).toLocaleDateString()}</span>}
-                                <span className="my-custom-block-category">{post.categories}</span>
-                                <a href={post.link}>{post.title.rendered}</a>
-                            </li>
-                        ))}
-                </ul>
-            </div>
-        );
-    },
+    edit: MyCustomBlockEdit,
     save() {
         return null;
     },
